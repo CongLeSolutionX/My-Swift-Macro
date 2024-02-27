f70caa2ef5c0be5e2b31410ac5f12c11b953dfe0
Cong Le <longchike@gmail.com>
2024-02-27 17:17:28 -0500

    - update

--- Sources/MyMacroClient/main.swift
+++ Sources/MyMacroClient/main.swift
@@ -6,3 +6,6 @@ let b = 25
 let (result, code) = #stringify(a + b)
 
 print("The value \(result) was produced by the code \"\(code)\"")
+
+
+//let invalidURL = #URL("invalid URL link!")
--- Sources/MyMacroMacros/MyMacroMacro.swift
+++ Sources/MyMacroMacros/MyMacroMacro.swift
@@ -2,6 +2,7 @@ import SwiftCompilerPlugin
 import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
+import Foundation
 
 /// Implementation of the `stringify` macro, which takes an expression
 /// of any type and produces a tuple containing the value of that expression
@@ -25,9 +26,54 @@ public struct StringifyMacro: ExpressionMacro {
     }
 }
 
+/// Implementation of the `URL` macro
+
+public struct URLMacro: ExpressionMacro {
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> ExprSyntax {
+        guard
+            /// 1. Grab the first (and only) Macro argument.
+            let argument = node.argumentList.first?.expression,
+            /// 2. Ensure the argument contains of a single String literal segment.
+            let segments = argument.as(StringLiteralExprSyntax.self)?.segments,
+            segments.count == 1,
+            /// 3. Grab the actual String literal segment.
+            case .stringSegment(let literalSegment)? = segments.first
+        else {
+            throw URLMacroError.requiresStaticStringLiteral
+        }
+
+        /// 4. Validate whether the String literal matches a valid URL structure.
+        guard let _ = URL(string: literalSegment.content.text) else {
+            throw URLMacroError.malformedURL(urlString: "\(argument)")
+        }
+
+        return "URL(string: \(argument))!"
+    }
+}
+
+/// Implementation of `URLMacroError`
+enum URLMacroError: Error, CustomStringConvertible {
+    case requiresStaticStringLiteral
+    case malformedURL(urlString: String)
+
+    var description: String {
+        switch self {
+        case .requiresStaticStringLiteral:
+            return "#URL requires a static string literal"
+        case .malformedURL(let urlString):
+            return "The input URL is malformed: \(urlString)"
+        }
+    }
+}
+
+
 @main
 struct MyMacroPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
         StringifyMacro.self,
+        URLMacro.self
     ]
 }
